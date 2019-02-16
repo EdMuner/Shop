8284cb9a9cd92c980a4482adcb787c15f01e1c2c
EdisonMunera <36096368+EdisonMunera@users.noreply.github.com>
2019-02-16 14:54:59 -0500

    Config BD

---
+++ Shop.Web/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace Shop.Web.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Entities;
+    using Microsoft.EntityFrameworkCore;
+   
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+
+}
