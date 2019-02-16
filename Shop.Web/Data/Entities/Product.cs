7922fefb24b7bd3f5f80304a0209ecd834b13e6e
EdisonMunera <36096368+EdisonMunera@users.noreply.github.com>
2019-02-16 16:31:18 -0500

    Configure add products to the DB  if it is empty

--- Shop.Web/Data/Entities/Product.cs
+++ Shop.Web/Data/Entities/Product.cs
@@ -12,7 +12,7 @@ namespace Shop.Web.Data.Entities
     {
         public int Id { get; set; }
 
-        [MaxLength(50)]
+        [MaxLength(50, ErrorMessage = "The fiel {0} only can contain {1} characters lenght.")]
         [Required]
         public string Name { get; set; }
 
