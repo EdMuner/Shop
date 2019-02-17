d25563f792428cf1651e4aaf2c8ad95b041b341a
EdisonMunera <36096368+EdisonMunera@users.noreply.github.com>
2019-02-16 22:05:06 -0500

    Add Users

--- Shop.Web/Data/DataContext.cs
+++ Shop.Web/Data/DataContext.cs
@@ -7,11 +7,14 @@ namespace Shop.Web.Data
     using System.Linq;
     using System.Threading.Tasks;
     using Entities;
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
    
 
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User>
     {
+
+
         public DbSet<Product> Products { get; set; }
 
         public DataContext(DbContextOptions<DataContext> options) : base(options)
