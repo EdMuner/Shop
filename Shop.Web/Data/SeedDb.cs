e866f090e40dba831f5dbf6988d27169b7119db8
EdisonMunera <36096368+EdisonMunera@users.noreply.github.com>
2019-02-16 22:33:14 -0500

    user implementation

--- Shop.Web/Data/SeedDb.cs
+++ Shop.Web/Data/SeedDb.cs
@@ -7,32 +7,54 @@ namespace Shop.Web.Data
     using System.Linq;
     using System.Threading.Tasks;
     using Entities;
+    using Microsoft.AspNetCore.Identity;
 
     public class SeedDb
     {
         private readonly DataContext context;
+        private readonly UserManager<User> userManager;
         private Random random;
 
-        public SeedDb(DataContext context)
+        public SeedDb(DataContext context, UserManager<User> userManager)
         {
+            this.userManager = userManager;
             this.context = context;
             this.random = new Random();
-        }
+        }       
 
         public async Task SeedAsync()
         {
             await this.context.Database.EnsureCreatedAsync();
 
+            var user = await this.userManager.FindByEmailAsync("edisonmunera72@gmail.com");
+            if (user == null)
+            {
+                user = new User
+                {
+                    FirstName = "Edi",
+                    LastName = "Munera",
+                    Email = "edisonmunera72@gmail.com",
+                    UserName = "edisonmunera72@gmail.com",
+                    PhoneNumber = "3147187953"
+                };
+
+                var result = await this.userManager.CreateAsync(user, "123456");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user in seeder");
+                }
+            }
+
             if (!this.context.Products.Any())
             {
-                this.AddProduct("iPhone XS");
-                this.AddProduct("Magic Mouse");
-                this.AddProduct("iWatch Series 4");
+                this.AddProduct("iPhone XS", user);
+                this.AddProduct("Magic Mouse", user);
+                this.AddProduct("iWatch Series 4", user);
                 await this.context.SaveChangesAsync();
             }
         }
 
-        private void AddProduct(string name)
+        private void AddProduct(string name, User user)
         {
             this.context.Products.Add(new Product
             {
