b8ecfb837e6a02ca0a8270e717a090b86bafc421
EdisonMunera <36096368+EdisonMunera@users.noreply.github.com>
2019-02-24 13:57:47 -0500

    Login android succesfull

--- Shop/Shop.UIForms/Shop.UIForms/App.xaml.cs
+++ Shop/Shop.UIForms/Shop.UIForms/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Shop.UIForms.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace Shop.UIForms
 {
     public partial class App : Application
@@ -10,7 +12,7 @@ namespace Shop.UIForms
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new LoginPage());
         }
 
         protected override void OnStart()
